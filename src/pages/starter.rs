30180644afce3d67d52d707735a2207e85b71757
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2022-07-14 19:15:56 +0800

    feat: add `onkeydown` for starter page

--- src/pages/starter.rs
+++ src/pages/starter.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use dioxus::{prelude::*, router::RouterCore};
+use dioxus::{prelude::*, router::RouterCore, web::use_eval};
 use dioxus_toast::ToastInfo;
 
 use crate::{
@@ -12,10 +12,11 @@ use crate::{
 };
 
 pub fn HelloDioxus(cx: Scope) -> Element {
-    let window = web_sys::window().unwrap();
     let input_name = use_state(&cx, String::new);
     let svc = cx.use_hook(|_| cx.consume_context::<Arc<RouterCore>>());
     let toast = use_atom_ref(&cx, TOAST_MANAGER);
+    let eval = use_eval(&cx);
+
     cx.render(rsx! {
         section {
             class: "h-screen bg-cover bg-white dark:bg-gray-600",
@@ -38,9 +39,19 @@ pub fn HelloDioxus(cx: Scope) -> Element {
                             class: "rounded-md border border-transparent bg-gray-800 dark:bg-white/20 px-4 py-2 text-white placeholder-white backdrop-blur-sm focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 sm:mx-2",
                             placeholder: "What's your name ?",
                             oninput: move |e| input_name.set(e.value.clone()),
+                            onkeydown: move |e| {
+                                match e.key_code {
+                                    dioxus::events::KeyCode::Enter => {
+                                        // if the enter key is down, we also need redirect to `hi/{path}`
+                                        eval("document.getElementById('submit-button').click()");
+                                    },
+                                    _ => {},
+                                }
+                            },
                             value: "{input_name}"
                         }
                         button {
+                            id: "submit-button",
                             class: "transform rounded-md bg-blue-700 px-8 py-2 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2",
                             onclick: move |_| {
                                 let name = input_name.get();
